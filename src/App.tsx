5c7dbb46169d02965db85285ac4bcb58e26053c3
Akin Karayun <akinkarayun1@hotmail.com>
2022-04-26 23:57:38 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <body>
+        <h1>Hello, My Name is Akin.</h1>
+        <p className='subtitle'>Welcome to my website</p>
+      </body>
     </div>
   );
 }
