e16e8cb9cd800e8b4e0d9cc3d8dad44ed69b3520
Akin Karayun <akinkarayun1@hotmail.com>
2022-04-28 00:18:37 +0300

    changing styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,26 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <body>
-        <h1>Hello, My Name is Akin.</h1>
-        <p className='subtitle'>Welcome to my website</p>
-      </body>
-    </div>
+    <body>
+      <h1>Hello, My Name is Akin.</h1>
+      <p className='subtitle'>Welcome to my website</p>
+      <div className='sociallinkContainer'>
+        <a href='https://medium.com/@akinkarayun' target={'_blank'}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Medium
+        </a>
+        <a href='https://www.linkedin.com/in/akin-karayun-ab3239bb/' target={'_blank'}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Linked In
+        </a>
+      </div>
+    </body >
   );
 }
 
